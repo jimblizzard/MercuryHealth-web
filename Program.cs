78245f9f1f5619d7fcd22b38f6e44edbd873a2d3
jimblizzard <jimblizzard@hotmail.com>
2021-07-15 11:01:49 -0400

    AB#411 Simple correction made

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
-// simple change 
+// simple change here 
 namespace AspNetCoreVueStarter
 {
     public class Program
